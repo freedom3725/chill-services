2d6c38836baf77b1965951c364fdbbdd1decaa8f
freedom3725 <chuping0725@gmail.com>
2021-06-05 14:43:50 +0800

    ㄅ

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v4.3.0 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Small Business v5.0.1 (https://startbootstrap.com/template/small-business)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
